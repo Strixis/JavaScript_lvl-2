924cca5b9111ed68fa482e11b6f21dae443b26a8
Strixis <nivem-strixis@mail.ru>
2020-10-08 21:55:58 +0300

    Добавлена визуализция заказа, версия 2

--- L-2_task-3/js/script.js
+++ L-2_task-3/js/script.js
@@ -3,25 +3,25 @@
 class Hamburger {
     constructor(container = '.hamburger') {
         this.container = container
-        this.allComponents = [];
-        this.components = [];
-        this.cost = null;
-        this.calorie = null;
+        this._allComponents = [];
+        this._components = [];
+        this._cost = null;
+        this._calorie = null;
         this._init();
-        document.querySelector(this.container).addEventListener('click', (event) => {this._containerClickHandler(event)});
     }
     
     _init() {
-        this._fetchAllComponents();
-        this.components = [{category: 'size', name: 'little', cost: 50, calorie: 20},
+        this.fetchAllComponents();
+        this._components = [{category: 'size', name: 'little', cost: 50, calorie: 20},
                            {category: 'filling', name: 'cheese', cost: 10, calorie: 20}];
-        this.cost = this._calculateCostAndCalorie().cost;
-        this.calorie = this._calculateCostAndCalorie().calorie;
+        this._cost = this._calculateCostAndCalorie().cost;
+        this._calorie = this._calculateCostAndCalorie().calorie;
         this._render();
+        document.querySelector(this.container).addEventListener('click', (event) => {this._containerClickHandler(event)});
     }
     
-    _fetchAllComponents() {
-        this.allComponents = [
+    fetchAllComponents() {
+        this._allComponents = [
             {category: 'size', name: 'little', cost: 50, calorie: 20},
             {category: 'size', name: 'big', cost: 100, calorie: 40},
             {category: 'filling', name: 'cheese', cost: 10, calorie: 20},
@@ -32,78 +32,128 @@ class Hamburger {
         ];
     }
     
+    _calculateCostAndCalorie(cost = 0, calorie = 0) {
+        for (let component of this._components) {
+            cost += component.cost;
+            calorie += component.calorie;
+        }
+        return {cost, calorie};
+    }
+    
+    _render(imgSelector = '.hamburger_img',
+            consistId = 'consist',
+            costId = 'total-cost',
+            calorieId = 'total-calorie') {
+        const imgElem = document.querySelector(imgSelector),
+              componentsNames = this._getComponentsNames();
+        
+        for (let name = 0; name < componentsNames.length; name++) {
+            switch (componentsNames[name]) {
+                case 'little':
+                    imgElem.style.width = 'auto';
+                    continue;
+                case 'big':
+                    imgElem.style.width = '150px';
+                    continue;
+                case 'cheese':
+                    imgElem.style.filter = 'hue-rotate(0deg)';
+                    continue;
+                case 'salad':
+                    imgElem.style.filter = 'hue-rotate(25deg)';
+                    continue;
+                case 'potatoes':
+                    imgElem.style.filter = 'hue-rotate(-25deg)';
+                    continue;
+                case 'sauce':
+                    imgElem.style.filter += 'opacity(0.8)';
+                    continue;
+                case 'mayonnaise':
+                    imgElem.style.filter += 'sepia(0.2)';
+                    continue;
+            }
+        }
+        
+        document.getElementById(consistId).innerHTML = componentsNames.join(' ');
+        document.getElementById(costId).innerHTML = this._cost + 'rub';
+        document.getElementById(calorieId).innerHTML = this._calorie + 'cal';
+    }
+    
+    _getComponentsNames() {
+        let names = [];
+        for (let component of this._components) {
+            names.push(component.name);
+        }
+        return names;
+    }
+    
     _containerClickHandler(event) {
         if  (event.target.tagName !== 'BUTTON' ) {
             return;
         } else {
-            const name = event.target.id;
-            const component = this._getComponentByName(name);
+            const componentName = event.target.id;
+            const component = this._getComponentByName(componentName);
             
-            if (name.startsWith('no-')) {
-                this._removeTopping(name);
-            } else if (name === 'buy') {
+            if (componentName.startsWith('no-')) {
+                this._removeTopping(componentName);
+            } else if (componentName === 'buy') {
                 this._takeIt();
             } else {
                 this._fetchOrReplaceComponent(component);
             }
             
-            this.cost = this._calculateCostAndCalorie().cost;
-            this.calorie = this._calculateCostAndCalorie().calorie;
+            this._cost = this._calculateCostAndCalorie().cost;
+            this._calorie = this._calculateCostAndCalorie().calorie;
             this._render();
         }
     }
     
+    _getComponentByName(name) {
+        return this._allComponents.find((component) => {return component.name === name});
+    }
+    
+    _removeTopping(name) {
+        if (name === 'no-topping') {
+            for (let index = 0; index < this._components.length; index++) {
+                if (this._components[index].category === 'topping') {
+                    this._components.splice(index, 1);
+                    index -= 1;
+                }
+            }
+        } else {
+            for (let index = 0; index < this._components.length; index++) {
+                if (`no-${this._components[index].name}` === name) {
+                    this._components.splice(index, 1);
+                    break;
+                }
+            }
+        }
+    }
+    
     _takeIt() {
-        let message = `You take ${this.components[0].name} hamburger with ${this.components[1].name}`,
+        let message = `You take ${this._components[0].name} hamburger with ${this._components[1].name}`,
             toppings = [];
-        for (let component of this.components) {
+        for (let component of this._components) {
             if (component.category === 'topping') {
                 toppings.push(component.name);
             }
         }
         if (toppings.length === 0) {
-            alert(`${message}.\nIt cost: ${this.cost}rub.\nIt callorie: ${this.calorie}cal.`);
+            alert(`${message}.\nIt cost: ${this._cost}rub.\nIt callorie: ${this._calorie}cal.`);
         } else {
-            alert(`${message}, ${toppings.join(', ')}.\nIt cost: ${this.cost}rub.\nIt callorie: ${this.calorie}cal.`)
+            alert(`${message}, ${toppings.join(', ')}.\nIt cost: ${this._cost}rub.\nIt callorie: ${this._calorie}cal.`)
         }
     }
     
     _fetchOrReplaceComponent(component) {
         if (this._isUnique(component) && (this._isTopping(component) || !this._isSameType(component))) {
-                this._fetchComponent(component);
+                this._components.push(component);
             } else {
                 this._replaceComponent(component);
             }
     }
     
-    _removeTopping(name) {
-        if (name === 'no-topping') {
-            for (let index = 0; index < this.components.length; index++) {
-                if (this.components[index].category === 'topping') {
-                    this.components.splice(index, 1);
-                    index -= 1;
-                }
-            }
-        } else {
-            for (let index = 0; index < this.components.length; index++) {
-                if (`no-${this.components[index].name}` === name) {
-                    this.components.splice(index, 1);
-                    break;
-                }
-            }
-        }
-    }    
-    
-    _getComponentByName(name) {
-        return this.allComponents.find((component) => {return component.name === name});
-    }
-    
-    _fetchComponent(newComponent) {
-        this.components.push(newComponent);
-    }
-    
     _isUnique(newComponent) {
-        for (let component of this.components) {
+        for (let component of this._components) {
             if (newComponent.name === component.name) {
                 return false;
             }
@@ -116,7 +166,7 @@ class Hamburger {
     }
     
     _isSameType(newComponent) {
-        for (let component of this.components) {
+        for (let component of this._components) {
             if (newComponent.category === component.category) {
                 return true;
             }
@@ -125,39 +175,15 @@ class Hamburger {
     }
     
     _replaceComponent(newComponent) {
-        for (let index = 0; index < this.components.length; index++) {
-            if (this._isTopping(this.components[index]) && this.components[index].name === newComponent.name) {
-                this.components.splice(index, 1, newComponent);
+        for (let index = 0; index < this._components.length; index++) {
+            if (this._isTopping(this._components[index]) && this._components[index].name === newComponent.name) {
+                this._components.splice(index, 1, newComponent);
                 break;
-            } else if (!this._isTopping(this.components[index]) && this.components[index].category === newComponent.category) {
-                this.components.splice(index, 1, newComponent);
+            } else if (!this._isTopping(this._components[index]) && this._components[index].category === newComponent.category) {
+                this._components.splice(index, 1, newComponent);
                 break;
             }
         }
-    }
-    
-    _calculateCostAndCalorie(cost = 0, calorie = 0) {
-        for (let component of this.components) {
-            cost += component.cost;
-            calorie += component.calorie;
-        }
-        return {cost, calorie};
-    }
-    
-    _render(consistId = 'consist',
-                           costId = 'total-cost',
-                           calorieId = 'total-calorie') {
-        document.getElementById(consistId).innerHTML = this._getComponentsNames();
-        document.getElementById(costId).innerHTML = this.cost + 'rub';
-        document.getElementById(calorieId).innerHTML = this.calorie + 'cal';
-    }
-    
-    _getComponentsNames() {
-        let names = [];
-        for (let component of this.components) {
-            names.push(component.name);
-        }
-        return names.join(' ');
     }
 }
 
