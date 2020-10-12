7c678dcff5fd59420f1cf16094d25dc63d987e19
Strixis <nivem-strixis@mail.ru>
2020-10-12 13:02:51 +0300

    Сделана практика к уроку

--- L-3/js/script.js
+++ L-3/js/script.js
@@ -1,5 +1,22 @@
 "use strict";
 
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+let getRequest = (url, cb) => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if(xhr.status !== 200) {
+                console.log('Error')
+            } else {
+                cb(xhr.responseText);
+            }
+        }
+    }
+    xhr.send();
+}
+
 /**
  * Класс, представляющий список товаров.
  */
@@ -13,41 +30,23 @@ class GoodsList {
         this.goods = [];
         this.allGoods = [];
         this.allGoodsCost = 0;
-        this._fetchGoods();
-        this._render();
-        this._fetchAllGoodsCost();
+        this._getGoods()
+            .then(data => {
+                this.goods = [...data];
+                this._render();
+                this._fetchAllGoodsCost();
+        });
     }
     
     /**
      * Получает список товаров.
      */
-    _fetchGoods() {
-        this.goods = [
-            {
-                imgAdress: undefined,
-                title: 'Shirt',
-                price: 150,
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In malesuada libero ultrices felis facilisis, in ornare tellus consequat. Pellentesque a.'
-            },
-            {
-                imgAdress: 'wrong adress',
-                title: 'Socks',
-                price: 50,
-                description: 'Consectetur adipiscing elit. In malesuada libero ultrices felis facilisis, in ornare tellus consequat. Pellentesque a.'
-            },
-            {
-                imgAdress: undefined,
-                title: 'Jacket',
-                price: 350,
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In malesuada, in ornare tellus consequat. Pellentesque a.'
-            },
-            {
-                imgAdress: undefined,
-                title: 'Shoes',
-                price: 250,
-                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In malesuada libero ultrices felis facilisis, in ornare tellus consequat.',
-            }
-        ];
+    _getGoods() {
+       return fetch(`${API}/catalogData.json`)
+           .then(response => response.json())
+           .catch(error => {
+           console.log('Error')
+       });
     }
     
     /**
@@ -67,9 +66,8 @@ class GoodsList {
      * Получает суммарную стоимость всех товаров.
      */
     _fetchAllGoodsCost() {
-        for (let good of this.allGoods) {
-            this.allGoodsCost += good.price;
-        }
+        this.allGoodsCost = this.allGoods.reduce((previousValue, currentValue) => {return previousValue + currentValue.price}, this.allGoodsCost);
+
         console.log(this.allGoodsCost);
     }
 }
@@ -85,9 +83,9 @@ class GoodsItem {
      */
     constructor(good) {
         this.imgAdress = good.imgAdress !== undefined ? good.imgAdress : 'img/sharp.jpeg';
-        this.title = good.title;
+        this.title = good.product_name;
         this.price = good.price;
-        this.description = good.description;
+        this.description = good.description !== undefined ? good.description : 'There is no description.';;
     }
     
     /**
