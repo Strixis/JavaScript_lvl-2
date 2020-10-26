64782d624e2b4b354c077663cc35c79d54d8b9f3
Strixis <nivem-strixis@mail.ru>
2020-10-26 20:56:48 +0300

    Сделан компонент для формы обратной связи

--- L-6/js/main.js
+++ L-6/js/main.js
@@ -4,12 +4,6 @@ const app = new Vue({
     el: '#app',
     data: {
         serverError: '',
-        
-        nameValue: '',
-        phoneValue: '',
-        mailValue: '',
-        commentValue: '',
-        validationResults: [true, true, true, true],
     },
     
     methods: {
@@ -21,35 +15,5 @@ const app = new Vue({
                     console.log(error);
             })
         },
-        validate(event) {
-            const results = [];
-            
-            results.push(this.isValidName);
-            results.push(this.isValidPhone);
-            results.push(this.isValidMail);
-            results.push(this.isValidComment);
-
-            this.validationResults = results;
-            if (!results.includes(false)) {
-                event.preventDefault();
-                alert('Данные отправлены!');
-            } else {
-                event.preventDefault();
-            }
-        }
-    },
-    computed: {
-        isValidName() {
-            return this.nameValue.match(/^[a-zA-Zа-яА-яЁё]+$/) === null ? false : true;
-        },
-        isValidPhone() {
-            return this.phoneValue.match(/^(\+\d\(\d{3}\)\d{3}-\d{4})$/) === null ? false : true;
-        },
-        isValidMail() {
-            return this.mailValue.match(/^[a-z]{2}[\.-]?[a-z]+@[a-z]+\.[a-z]{2,6}$/) === null ? false : true;
-        },
-        isValidComment() {
-            return this.commentValue.match(/.+/) === null ? false : true;
-        }
     },
 });
