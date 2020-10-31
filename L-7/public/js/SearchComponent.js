6afe176369aeca79bd2b8281ed095ea61a54350f
Strixis <nivem-strixis@mail.ru>
2020-10-31 18:57:47 +0300

    Сделана практика для урока 7

---
+++ L-7/public/js/SearchComponent.js
@@ -0,0 +1,21 @@
+Vue.component('search', {
+    data() {
+        return {
+            searchLine: '',
+        };
+    },
+    methods: {
+        filterProducts() {
+            const porductListAPI = this.$root.$refs['product-list'];
+            if (this.searchLine === '') {
+                porductListAPI.products =  porductListAPI.productsFromServer;
+            } else {
+                porductListAPI.products = porductListAPI.productsFromServer.filter((product) => product.product_name.toLowerCase().includes(this.searchLine.toLowerCase()));
+            }
+        },
+    },
+    template: `<form action="" class="search-form">
+                    <input type="text" class="search-form_search-field" v-model.lazy="searchLine">
+                    <input type="submit" class="search-form_submit-button" value="Search" @click.prevent="filterProducts">
+                </form>`
+})
---
+++ L-7/public/js/main.js
@@ -0,0 +1,45 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        serverError: '',
+    },
+    
+    methods: {
+        getJson(url) {
+            return fetch(url)
+               .then(response => response.json())
+               .catch(error => {
+                    this.serverError = error;
+                    console.log(error);
+            })
+        },
+        postJson(url, data){
+            return fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+                .catch(error => {
+                    this.$refs.error.text = error;
+                })
+        },
+        putJson(url, data){
+            return fetch(url, {
+                method: 'PUT',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+                .catch(error => {
+                    this.$refs.error.text = error;
+                })
+        },
+    },
+});
